6d2cd582576c850675beaff8b5c97d2877cd1727
Louis13131 <antipenkovalexandr1993@gmail.com>
2025-11-17 18:50:00 +0300

    Тесты для Game вынесены в отдельный фалй

--- src/test/java/ru/urfu/ConsoleInteractionTest.java
+++ src/test/java/ru/urfu/ConsoleInteractionTest.java
@@ -13,67 +13,6 @@ public class ConsoleInteractionTest {
     }
 
     @Test
-    public void getAnswerInGameInfoLidiaTest() {
-        Game game = new Game();
-        String result = game.processCommandInGame("/info_lidia");
-        String expected = String.format("--- Информация о %s %s --- \n%s лет \nРод занятий: %s \nОписание: %s \nМотив: %s", Strings.lidia);
-        Assertions.assertEquals(expected, result);
-    }
-
-    @Test
-    public void getAnswerInGameInfoAnnaTest() {
-        Game game = new Game();
-        String result = game.processCommandInGame("/info_anna");
-        String expected = String.format("--- Информация о %s %s --- \n%s лет \nРод занятий: %s \nОписание: %s \nМотив: %s", Strings.anna);
-        Assertions.assertEquals(expected, result);
-    }
-
-    @Test
-    public void getAnswerInGameInfoDmitriyTest() {
-        Game game = new Game();
-        String result = game.processCommandInGame("/info_dmitriy");
-        String expected = String.format("--- Информация о %s %s --- \n%s лет \nРод занятий: %s \nОписание: %s \nМотив: %s", Strings.dmitriy);
-        Assertions.assertEquals(expected, result);
-    }
-
-    @Test
-    public void getAnswerInGameInfoPetrTest() {
-        Game game = new Game();
-        String result = game.processCommandInGame("/info_petr");
-        String expected = String.format("--- Информация о %s %s --- \n%s лет \nРод занятий: %s \nОписание: %s \nМотив: %s", Strings.petr);
-        Assertions.assertEquals(expected, result);
-    }
-
-    @Test
-    public void getAnswerInGameInfoGrigoriyTest() {
-        Game game = new Game();
-        String result = game.processCommandInGame("/info_grigoriy");
-        String expected = String.format("--- Информация о %s %s --- \n%s лет \nРод занятий: %s \nОписание: %s \nМотив: %s", Strings.grigoriy);
-        Assertions.assertEquals(expected, result);
-    }
-
-    @Test
-    public void getAnswerInGameExitTest() {
-        Game game = new Game();
-        String result = game.processCommandInGame("/exit");
-        Assertions.assertEquals("Игра завершена", result);
-    }
-
-    @Test
-    public void getAnswerInGameIncorrectCommandTest() {
-        Game game = new Game();
-        String result = game.processCommandInGame("/incorrectCommand");
-        Assertions.assertEquals("Такой команды не существует", result);
-    }
-
-    @Test
-    public void getAnswerInGameStart_gameTest() {
-        Game game = new Game();
-        String result = game.processCommandInGame("/start_game");
-        Assertions.assertEquals("Игра уже началась", result);
-    }
-
-    @Test
     public void getAnswerBeforeHelpTest() {
         ProcessCommand processCommand = new ProcessCommand();
         String result = processCommand.getAnswerBeforeGame("/help");
