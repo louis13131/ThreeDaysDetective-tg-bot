373dbcc0d9c43638f23793e3f2dad955a628ae94
Louis13131 <antipenkovalexandr1993@gmail.com>
2025-11-18 07:25:15 +0300

    Добавлена возможность запускать Telegram и консоль бота в зависимости от аргументов программы

--- src/main/java/ru/urfu/Main.java
+++ src/main/java/ru/urfu/Main.java
@@ -5,6 +5,13 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
         ProcessMain processMain = new ProcessMain();
-        processMain.run();
+        if  (args.length != 1) {
+            String defaultRunning = "both";
+            processMain.run(defaultRunning);
+        }
+        else {
+            String running = args[0];
+            processMain.run(running);
+        }
     }
 }
